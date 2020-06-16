8c1cf19c949db89bac98e0493b19f0d3b0a72e80
Saptarshi <roni.bagchi@gmail.com>
2020-06-16 13:54:40 +0530

    Added the trimmer activity

--- app/src/main/java/com/roni/videoflow/TrimmerActivity.java
+++ app/src/main/java/com/roni/videoflow/TrimmerActivity.java
@@ -1,13 +1,70 @@
 package com.roni.videoflow;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class TrimmerActivity extends AppCompatActivity {
+import life.knowledge4.videotrimmer.K4LVideoTrimmer;
+import life.knowledge4.videotrimmer.interfaces.OnTrimVideoListener;
+
+public class TrimmerActivity extends AppCompatActivity implements OnTrimVideoListener{
+
+    private K4LVideoTrimmer mVideoTrimmer;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trimmer);
+        Intent extraIntent = getIntent();
+        String path = "";
+
+        if (extraIntent != null) {
+            path = extraIntent.getStringExtra(MainActivity.EXTRA_VIDEO_PATH);
+        }
+
+        //setting progressbar
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setMessage("Trimming in progress");
+
+        mVideoTrimmer = ((K4LVideoTrimmer) findViewById(R.id.timeLine));
+        if (mVideoTrimmer != null) {
+            mVideoTrimmer.setMaxDuration(10);
+            mVideoTrimmer.setOnTrimVideoListener(this);
+           // mVideoTrimmer.setOnK4LVideoListener(this);
+            //mVideoTrimmer.setDestinationPath("/storage/emulated/0/DCIM/CameraCustom/");
+            mVideoTrimmer.setVideoURI(Uri.parse(path));
+          //  mVideoTrimmer.setVideoInformationVisibility(true);
+        }
     }
+
+    @Override
+    public void getResult(final Uri uri) {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(TrimmerActivity.this, "Video at : " + uri.getPath(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setDataAndType(uri, "video/mp4");
+        startActivity(intent);
+        finish();
+
+    }
+
+    @Override
+    public void cancelAction() {
+
+        mVideoTrimmer.destroy();
+        finish();
+
+    }
+
+
 }
