42a602f336c800a362c52d7ba72004d8d3e1bebb
Saptarshi <roni.bagchi@gmail.com>
2020-07-02 16:49:52 +0530

    Added Streaming over HTTP

--- app/src/main/java/com/roni/videoflow/FeedConcept/VideoFullScreenActivity.java
+++ app/src/main/java/com/roni/videoflow/FeedConcept/VideoFullScreenActivity.java
@@ -1,12 +1,30 @@
 package com.roni.videoflow.FeedConcept;
-
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-
+import android.os.Handler;
+import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.roni.videoflow.Adapters.VideoItemAdapter;
 import com.roni.videoflow.Models.VideoItemModel;
 import com.roni.videoflow.R;
@@ -16,9 +34,46 @@ import java.util.ArrayList;
 
 public class VideoFullScreenActivity extends AppCompatActivity {
 
+    private PlayerView playerView;
+    private SimpleExoPlayer simpleExoPlayer;
+
+
+    private static final String TAG = "VideoFullScreenActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_full_screen);
+        playerView = (PlayerView) findViewById(R.id.playerView);
+
+        //the incoming intent
+        Intent i = getIntent();
+        VideoItemModel currentModel = (VideoItemModel)i.getSerializableExtra("item");
+        Log.i(TAG, "onCreate: " + currentModel.getVideoURL());
+
+        simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(this);
+        playerView.setPlayer(simpleExoPlayer);
+        DefaultBandwidthMeter defaultBandwidthMeter = new DefaultBandwidthMeter();
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "VideoFlow"), defaultBandwidthMeter);
+
+       MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(currentModel.getVideoURL()));
+
+        simpleExoPlayer.prepare(videoSource);
+        simpleExoPlayer.setPlayWhenReady(true);
+        
+
+
+
+
     }
 
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        simpleExoPlayer.release();
+    }
 }
--- app/src/main/java/com/roni/videoflow/Repository/VideoItems.java
+++ app/src/main/java/com/roni/videoflow/Repository/VideoItems.java
@@ -7,18 +7,17 @@ import java.util.ArrayList;
 public class VideoItems {
 
 
-
-    public static ArrayList<VideoItemModel> getItems(){
+    public static ArrayList<VideoItemModel> getItems() {
         ArrayList<VideoItemModel> items = new ArrayList<>();
 
-       VideoItemModel currentItem = new VideoItemModel();
-       currentItem.setVideoURL("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
-       currentItem.setVideoTitle("Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself.");
-       currentItem.setThumbnailUrl("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg");
-       currentItem.setUsername("Big Buck Bunny");
-       items.add(currentItem);
+        VideoItemModel currentItem = new VideoItemModel();
+        currentItem.setVideoURL("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+        currentItem.setVideoTitle("Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself.");
+        currentItem.setThumbnailUrl("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg");
+        currentItem.setUsername("Big Buck Bunny");
+        items.add(currentItem);
 
-       currentItem = new VideoItemModel();
+        currentItem = new VideoItemModel();
         currentItem.setVideoURL("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4");
         currentItem.setVideoTitle("The first Blender Open Movie from 2006");
         currentItem.setThumbnailUrl("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg");
@@ -32,6 +31,21 @@ public class VideoItems {
         currentItem.setUsername("For Bigger Blazes");
         items.add(currentItem);
 
+
+        currentItem = new VideoItemModel();
+        currentItem.setVideoURL("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4");
+        currentItem.setVideoTitle("Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender.");
+        currentItem.setThumbnailUrl("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg");
+        currentItem.setUsername("Sintel");
+        items.add(currentItem);
+
+
+        currentItem = new VideoItemModel();
+        currentItem.setVideoURL("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4");
+        currentItem.setVideoTitle("Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.");
+        currentItem.setThumbnailUrl("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/SubaruOutbackOnStreetAndDirt.jpg");
+        currentItem.setUsername("By Garage419");
+        items.add(currentItem);
 
 
         return items;
